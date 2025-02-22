dcc84ff3eff051b8adcb9b2ac19b4812ff0cfe00
Aaron Shrimpton <aaronds@gmail.com>
2025-02-22 22:46:07 +0000

    Pin mappings for devikitc

--- main/main.c
+++ main/main.c
@@ -191,8 +191,8 @@ void app_main(void)
         .uart = {
             .rw_timeout_ms = 500,
             .port = UART_NUM_0,
-            .rx_gpio = UART_PIN_NO_CHANGE,
-            .tx_gpio = UART_PIN_NO_CHANGE,
+            .rx_gpio = 21,
+            .tx_gpio = 22,
         }
     };
 
